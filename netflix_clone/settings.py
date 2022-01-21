d7c253a9f7ad02d564ecd399b2d1f4fc309010bd
seongolee <seongo0521@gmail.com>
2022-01-21 19:08:01 +0900

    추가 - netflix_user DB까지 기본 셋팅

--- netflix_clone/settings.py
+++ netflix_clone/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.UserModel'
---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class UserModel(AbstractUser):
+    class Meta:
+        db_table = "netflix_user"
+
+    bio = models.CharField(max_length=256, default='')
+
