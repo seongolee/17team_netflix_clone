10ba65d0edcbd8d6af155963f713256455ea43e7
YoonSoeHyeon <ysh0714@naver.com>
2022-01-25 23:51:10 +0900

    no message

--- user/models.py
+++ user/models.py
@@ -1,11 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 # Create your models here.
 class UserModel(AbstractUser):
     class Meta:
-        db_table = "netflix_user"
+        db_table = "UserModel"
 
-    bio = models.CharField(max_length=256, default='')
+    # bio = models.CharField(max_length=256, default='')  # 상태메세지
+    # follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')
+
+
+    phone_number = models.CharField(max_length=30, blank=False)
+    country_code = models.CharField(max_length=256, default='ko')
+
+
+class ProfileId(models.Model):
+    class Meta:
+        db_table = "ProfileId"
+
+    profile_id =  models.CharField(max_length=256, blank=False)
+    profile_name = models.CharField(max_length=256, blank=False)
+    # bookimages = models.ImageField(upload_to='static/bookimages',default='None.jpg')
+    profile_image = models.ImageField(upload_to='', default='')
+    profile_can_use = models.BooleanField(default=True)
+    kids_or_not = models.BooleanField(default=False)
+
 
--- user/views.py
+++ user/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from .models import UserModel,ProfileId
+from django.contrib.auth import get_user_model
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def create_profile():
+
+    user = UserModel.objects.create_user(username=username, password=password, email =email)
+    for i in range(5):
+        ProfileId.objects.create(profile_id=user.userid +i)
