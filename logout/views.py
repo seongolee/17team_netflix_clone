05f3cde06842ef0d39b7cc75270579be0665bfff
YoonSoeHyeon <ysh0714@naver.com>
2022-01-25 22:32:20 +0900

    no message

--- logout/views.py
+++ logout/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def logout_view(request):
     return render(request,'logout/logout_page.html')
+
+# @login_required             # 사용자가 로그인이 꼭 되어있어야만 접근이 가능한 함수라는 뜻
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
