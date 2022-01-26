7ee138d45b2b0dc758825e16df52e8b49954109c
seongolee <seongo0521@gmail.com>
2022-01-26 23:03:50 +0900

    추가 - DB 모델링 추가

--- user/models.py
+++ user/models.py
@@ -8,10 +8,6 @@ class UserModel(AbstractUser):
     class Meta:
         db_table = "user_model"
 
-    # bio = models.CharField(max_length=256, default='')  # 상태메세지
-    # follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')
-
-
     phone_number = models.CharField(max_length=30, default='')
     country_code = models.CharField(max_length=256, default='ko')
 
@@ -20,26 +16,37 @@ class ProfileId(models.Model):
     class Meta:
         db_table = "profile_id"
 
-    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-
-
+    user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE, db_column='user_id')
     profile_id = models.CharField(max_length=256)
     profile_name = models.CharField(max_length=256)
-    # bookimages = models.ImageField(upload_to='static/bookimages',default='None.jpg')
+    # profile_image = models.ImageField(upload_to='static/bookimages',default='None.jpg')
     profile_image = models.ImageField(upload_to='', default='')
-    profile_can_use = models.BooleanField(default=True)
+    profile_can_use = models.BooleanField(default=None)
     kids_or_not = models.BooleanField(default=False)
 
 
-
-class Video(models.Model):
+class FooterInfo(models.Model):
     class Meta:
-        db_table = "profile_id"
+        db_table = "footer_info"
 
-    profile_id =  models.CharField(max_length=256)
-    profile_name = models.CharField(max_length=256)
-    # bookimages = models.ImageField(upload_to='static/bookimages',default='None.jpg')
-    profile_image = models.ImageField(upload_to='', default='')
-    profile_can_use = models.BooleanField(default=True)
-    kids_or_not = models.BooleanField(default=False)
+    # 법인이름
+    company_name = models.CharField(max_length=256)
+    # 통신판매업신고번호
+    report_number = models.CharField(max_length=256)
+    # 전화번호
+    company_number = models.CharField(max_length=256)
+    # 대표이름
+    Representative_name = models.CharField(max_length=256)
+    # 이메일주소
+    company_email = models.CharField(max_length=256)
+    # 법인주소지
+    company_address = models.CharField(max_length=256)
+    # 사업자등록번호
+    Business_number = models.CharField(max_length=256)
+
+
+
+
+
+
 
