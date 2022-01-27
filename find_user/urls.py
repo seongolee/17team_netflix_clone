7483ae36bf6c58ad507d45ee927b03640975ecda
YoonSoeHyeon <ysh0714@naver.com>
2022-01-27 20:20:47 +0900

    추가 - 인증번호 입력페이지 프론트엔드 완료

--- find_user/urls.py
+++ find_user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('find-user/', views.find_user_view, name='find-user'),
+    path('', views.find_user_view, name='find-user'),
+    path('certi/',views.certi_num_view, name='certi-num'),
 ]
--- find_user/views.py
+++ find_user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def find_user_view(request):
     return render(request,'find_pw/find_pw_page.html')
+
+def certi_num_view(request):
+    return render(request,'find_pw/certi_num_page.html')
