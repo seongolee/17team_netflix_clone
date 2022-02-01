2596604ab75f4263042f9104b495e9bfaa3c6d1f
JerryKim <ecec1023@gmail.com>
2022-02-02 07:23:10 +0900

    수정 - 로그인 ui 수정 및 json 가져오는 test 내용 삭제!

--- login/urls.py
+++ login/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     # 카카오로그인 설정
     path('', views.index, name='kakao'),
-    path('test/', views.test),
     path('kakaoLoginLogic/', views.kakaoLoginLogic),
     path('kakaoLoginLogicRedirect/', views.kakaoLoginLogicRedirect),
     path('kakaoLogout/', views.kakaoLogout),
