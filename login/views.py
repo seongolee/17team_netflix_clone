0f6c27116a4cc4be5390bc6286ac8666a7dcbdeb
JerryKim <ecec1023@gmail.com>
2022-02-02 19:15:27 +0900

    수정 - restapi 키네임 수정(깃헙PR 이슈때문에) / 로그인페이지에서  지금가입하세요 클릭시 가입하는 첫페이지로 이동하게 만듦. / 로그인 ui 수정

--- login/views.py
+++ login/views.py
@@ -86,9 +86,9 @@ def kakaoLoginLogic(request):
     return redirect(_url)
 def kakaoLoginLogicRedirect(request):
     _qs = request.GET['code']
-    _restApiKey = '046d90e62bcc4496f71f0dbc804694f8'
+    _restApi = '046d90e62bcc4496f71f0dbc804694f8'
     _redirect_uri = 'http://127.0.0.1:8000/main/'
-    _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
+    _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApi}&redirect_uri={_redirect_uri}&code={_qs}'
     _res = requests.post(_url)
     _result = _res.json()
     request.session['access_token'] = _result['access_token']
--- static/js/login/checkbox_save.js
+++ static/js/login/checkbox_save.js
@@ -2,16 +2,13 @@
 // id_password
 // bxid_rememberMe_true
 //
-//
 // 인터넷에서 범용적으로 돌고 있는 예제이다. 쿠키를 7일 동안 심거나 쿠키를 가져오거나 삭제한다.
 
 $(document).ready(function(){
 
     // 저장된 쿠키값을 가져와서 ID 칸에 넣어준다. 없으면 공백으로 들어감.
-    let idkey = getCookie("idkey");
-    $("#id_userLoginId").val(idkey);
-    let passwordkey = getCookie("passwordkey");
-    $("#id_password").val(passwordkey);
+    let key = getCookie("key");
+    $("#id_userLoginId").val(key);
 
     if($("#id_userLoginId").val() != ""){ // 그 전에 ID를 저장해서 처음 페이지 로딩 시, 입력 칸에 저장된 ID가 표시된 상태라면,
         $("#bxid_rememberMe_true").attr("checked", true); // ID 저장하기를 체크 상태로 두기.
@@ -19,17 +16,15 @@ $(document).ready(function(){
 
     $("#bxid_rememberMe_true").change(function(){ // 체크박스에 변화가 있다면,
         if($("#bxid_rememberMe_true").is(":checked")){ // 체크박스 체크했을 때,
-            setCookie("idkey", $("#id_userLoginId").val(), 7); // 7일 동안 쿠키 보관
-            setCookie("passwordkey", $("#id_password").val(), 7); // 7일 동안 쿠키 보관
+            setCookie("key", $("#id_userLoginId").val(), 7); // 7일 동안 쿠키 보관
         }else{ // ID 저장하기 체크 해제 시,
-            deleteCookie("idkey","passwordkey");
+            deleteCookie("key");
         }
     });
     // ID 저장하기를 체크한 상태에서 ID를 입력하는 경우, 이럴 때도 쿠키 저장.
     $("#id_userLoginId").keyup(function(){ // ID 입력 칸에 ID를 입력할 때,
         if($("#bxid_rememberMe_true").is(":checked")){ // 체크박스를 체크한 상태라면,
-            setCookie("idkey", $("#id_userLoginId").val(), 7); // 7일 동안 쿠키 보관
-            setCookie("passwordkey", $("#id_password").val(), 7); // 7일 동안 쿠키 보관
+            setCookie("key", $("#id_userLoginId").val(), 7); // 7일 동안 쿠키 보관
         }
     });
 });
@@ -58,99 +53,3 @@ function getCookie(cookieName) {
     }
     return unescape(cookieValue);
 }
-
-
-
-
-
-
-// 연습예제
-// $(function () {
-//
-// // 쿠키값을 가져온다.
-//     let cookie_user_id = getLogin();
-//
-//     /**
-//
-//      * 쿠키값이 존재하면 id에 쿠키에서 가져온 id를 할당한 뒤
-//
-//      * 체크박스를 체크상태로 변경
-//      */
-//     if (cookie_user_id != "") {
-//         $("#id_userLoginId").val(cookie_user_id);
-//         $("#bxid_rememberMe_true").attr("checked", true);
-//     }
-//
-// // 아이디 저장 체크시
-//     $("#bxid_rememberMe_true").on("click", function () {
-//         let _this = this;
-//         let isRemember;
-//         if ($(_this).is(":checked")) {
-//             isRemember = confirm("이 PC에 로그인 정보를 저장하시겠습니까? PC방등의 공공장소에서는 개인정보가 유출될 수 있으니 주의해주십시오.");
-//             if (!isRemember)
-//                 $(_this).attr("checked", false);
-//         }
-//     });
-//
-// // 로그인 버튼 클릭시
-//
-//     $("#btn_login").on("click", function () {
-//         if ($("#bxid_rememberMe_true").is(":checked")) { // 저장 체크시
-//             saveLogin($("#user_id").val());
-//         } else { // 체크 해제시는 공백
-//             saveLogin("");
-//         }
-//     });
-// });
-//
-//
-// /**
-//  * saveLogin
-//  * 로그인 정보 저장
-//  * @param id
-//  */
-//
-// function saveLogin(id) {
-//     if (id != "") {
-// // userid 쿠키에 id 값을 7일간 저장
-//         setSave("userid", id, 7);
-//     } else {
-// // userid 쿠키 삭제
-//         setSave("userid", id, -1);
-//     }
-// }
-//
-//
-// /**
-//  * setSave
-//  * Cookie에 user_id를 저장
-//  * @param name
-//  * @param value
-//  * @param expiredays
-//  */
-//
-// function setSave(name, value, expiredays) {
-//     let today = new Date();
-//     today.setDate(today.getDate() + expiredays);
-//     document.cookie = name + "=" + escape(value) + "; path=/; expires=" + today.toGMTString() + ";"
-// }
-// /**
-//  * getLogin
-//  * 쿠키값을 가져온다.
-//  * @returns {String}
-//  */
-//
-// function getLogin() {
-// // userid 쿠키에서 id 값을 가져온다.
-//     let cook = document.cookie + ";";
-//     let idx = cook.indexOf("userid", 0);
-//     let val = "";
-//
-//     if (idx != -1) {
-//         cook = cook.substring(idx, cook.length);
-//         begin = cook.indexOf("=", 0) + 1;
-//         end = cook.indexOf(";", begin);val = unescape(cook.substring(begin, end));
-//     }
-//
-//     return val;
-// }
