234bf5288cd16e82eb304f8f2a28ce773a1534b2
JerryKim <ecec1023@gmail.com>
2022-02-04 23:26:41 +0900

    추가 - 검색창 및 검색 후 페이지 / 메인 상단 헤더부분 수정 / 각종 기능

--- static/js/mainpage_js/main.js
+++ static/js/mainpage_js/main.js
@@ -9,6 +9,8 @@ fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
 
 // const modal = document.getElementById("modal")
 
+
+
 function modalOn() {
     const modal = document.getElementById("modal")
     modal.style.display = "flex"
@@ -52,3 +54,119 @@ window.addEventListener("keyup", e => {
         modalOff()
     }
 })
+
+
+// 우측 상단 2nd 네비 열고 닫기
+
+
+// $('#account-dropdown-button').on('change', function() {
+  //  alert( this.value ); // or $(this).val()
+// $(document).ready(function(){
+//     $('#sub-menu').hide();
+// });
+//
+// Toggle = true;
+//
+//
+// let dropdownlist = `<div role="menu" tabindex="0" class="account-drop-down sub-menu theme-lakira"
+//                                          style="opacity: 1; transition-duration: 150ms;">
+//                                         <div class="ptrack-content"
+//                                              data-ui-tracking-context="%7B%22appView%22:%22accountDropdownPanel%22%7D"
+//                                              data-tracking-uuid="8ae535ba-cbb2-4db9-a60e-247eb5dfdec7">
+//                                             <div class="topbar"></div>
+//                                             <ul class="sub-menu-list profiles" role="list" aria-label="프로필">
+//                                                 <li class="sub-menu-item profile" role="listitem">
+//                                                     <div><a class="profile-link" tabindex="0"
+//                                                             href="/SwitchProfile?tkn=XYQMPBXLEBAWTCAR6ZGHFI6GXA"
+//                                                             data-uia="action-select-profile+primary">
+//                                                         <div class="avatar-wrapper"><img class="profile-icon"
+//                                                                                          src="https://occ-0-3077-993.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABW6CcyxMhswxsXLwQIU0aOSE3fbpOWawb9JenXFNMJgF5uXHTW2eG_gqqoTnw6CYl6XPW60UvrQnjsE8hPKwQMU.png?r=552"
+//                                                                                          alt=""></div>
+//                                                         <span class="profile-name">김지용</span></a>
+//                                                         <div class="profile-children"></div>
+//                                                     </div>
+//                                                 </li>
+//                                                 <li class="sub-menu-item profile" role="listitem">
+//                                                     <div><a class="profile-link" tabindex="0"
+//                                                             href="/SwitchProfile?tkn=SLXQHUKPMVE2PMISJS5QGZ4RXU"
+//                                                             data-uia="action-select-profile+secondary">
+//                                                         <div class="avatar-wrapper"><img class="profile-icon"
+//                                                                                          src="https://occ-0-3077-993.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABXywvdPHBhL7h0TOuNr2uURK28yBKhjImyfjzYLWmUOB3qbQLUsIIYd9maV-kCAB2CqWulEzCApnRtm09vQoD6M.png?r=6f4"
+//                                                                                          alt=""></div>
+//                                                         <span class="profile-name">김지연</span></a>
+//                                                         <div class="profile-children"></div>
+//                                                     </div>
+//                                                 </li>
+//                                                 <li class="sub-menu-item profile-link" role="listitem"><a
+//                                                         aria-label="프로필 관리" class="sub-menu-link"
+//                                                         href="/profiles/manage">프로필 관리</a></li>
+//                                             </ul>
+//                                             <ul class="sub-menu-list responsive-links"></ul>
+//                                             <ul class="account-links sub-menu-list" aria-label="계정">
+//                                                 <li class="sub-menu-item"><a class="sub-menu-link" href="/YourAccount">계정</a>
+//                                                 </li>
+//                                                 <li class="sub-menu-item"><a class="sub-menu-link"
+//                                                                              href="https://help.netflix.com/">고객 센터</a>
+//                                                 </li>
+//                                                 <li class="sub-menu-item"><a class="sub-menu-link"
+//                                                                              href="/logout">넷플릭스에서 로그아웃</a>
+//                                                 </li>
+//                                             </ul>
+//                                         </div>
+//                                     </div>
+// `
+// function creat_function(){
+//     if(aria-expanded=false){
+//         $('#account-dropdown-button').append(dropdownlist);
+//         aria-expanded=true
+//     }
+//     else{
+//         $('#account-dropdown-button').remove(dropdownlist)
+//         aria-expanded=false
+//     }
+// }
+// function delete_function()
+// {
+//         if Toggle == True:
+//         Toggle = false;
+//         return Toggle
+//     $('#account-dropdown-button').remove(dropdownlist)
+// }
+//
+//
+//
+// drop = document.getElementsByClassName("sub-menu");
+ function dropdownshow(){
+     let drop =  document.getElementById("dropdown");
+     if(drop.style.display=='none'){
+		drop.style.display = 'block';
+        console.log('나는빡빡이다!')
+	}else{
+		drop.style.display = 'none';
+        console.log('나의 모발은 빽빽이다!')
+	}
+}
+
+// var bDisplay = true;
+// function doDisplay(){
+// 	var con = document.getElementById("myDIV");
+// 	if(con.style.display=='none'){
+// 		con.style.display = 'block';
+// 	}else{
+// 		con.style.display = 'none';
+// 	}
+// }
+// jQuery로 dropdown
+
+
+
+// let temp_html = `<div class="col">
+//                                             <div class="card">
+//                                                 <img src="${img_url}"
+//                                                      class="card-img-top" alt="...">
+//                                                 <div class="card-body" onclick="window.open('${url}')">
+//                                                     <h5 class="card-title">${title}</h5>
+//                                                 </div>
+//                                             </div>
+//                                         </div>`
+//                         $('#foot3').append(temp_html)
---
+++ static/js/mainpage_js/search.js
@@ -0,0 +1,73 @@
+// $(document).ready(function () {
+//             showvideo();
+//         });
+//
+//
+//
+//
+//
+// function showvideo() {
+//             // 중간 메인
+//             $.ajax({
+//                 type: "GET",
+//                 url: "/video/re",
+//                 data: {},
+//                 success: function (response) {
+//                     let video = response['videos']
+//                     for (let i = 0; i < prodct.length; i++) {
+//                         let title = prodct[i]['title']
+//                         let img_url = prodct[i]['img']
+//                         let price = prodct[i]['price']
+//                         let url = prodct[i]['url']
+//
+//                         let temp_html = `<div class="col" id="search_result">
+//                                             <div class="card h-100" >
+//                                                 <img class="card-img-top"
+//                                                      src="${img_url}"
+//                                                      alt="Card image cap">
+//                                                 <div class="heart">
+//                                                     <svg onclick="likeheart('${title}')" class="like" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+//                                                         <path d="M0 0h24v24H0z" fill="none"/>
+//                                                         <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+//                                                     </svg>
+//                                                 </div>
+//                                                 <div class="card-body" onclick="window.open('${url}')">
+//                                                     <h5 class="card-text">${title}</h5>
+//                                                     <p class="price">${price}</p>
+//                                                 </div>
+//                                             </div>
+//                                         </div>`
+//                         $('#fullPage').append(temp_html)
+//                     }
+//                 }
+//
+//             })
+//
+//         }
+//
+//
+//
+//                //검색 기능
+//         $(document).ready(function () {
+//             $("#searchQueryInput").keyup(function () {
+//                 var searchText = $(this).val();
+//                 // $("#fullPage > div > div").hide();
+//                 var temp = $("#fullPage > div").filter(function () {
+//                     $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1)
+//                 });
+//                 $(temp).parent().show();
+//             });
+//         });
+
+
+
+// 우측 상단 네비 창 드랍다운
+ // let drop = document.getElementsByClassName("sub-menu");
+ // function dropdownshow(){
+ //            if(drop.css('display') == 'none'){
+ //            drop.show();
+ //            }
+ //            else{
+ //            drop.hide();
+ //        }
+ // }
