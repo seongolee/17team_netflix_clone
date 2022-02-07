7d441adb653d4ee6213c6efe93414bf6e32a47e2
YoonSoeHyeon <ysh0714@naver.com>
2022-02-07 18:21:49 +0900

    수정 - 메인페이지 썸네일 완성

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('main/', views.main_view, name='main'),
     path('search/', views.search, name='search'),
     path('show-col/', views.showColumn, name='show-col'),
+    # path('show-genre/', views.show_genre, name='show-genre'),
     # path('search_enter/', views.SearchView, name='search_enter'),
 ]
